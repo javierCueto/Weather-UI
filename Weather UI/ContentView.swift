073ba5e1d2fe33b847d2fcf15aadf47692d64e64
Javier <javelie@outlook.com>
2020-03-21 18:49:00 -0700

    Initial Commit

---
+++ Weather UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather UI
+//
+//  Created by José Javier Cueto Mejía on 21/03/20.
+//  Copyright © 2020 WH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
